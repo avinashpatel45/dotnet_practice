820f1ddde0feb8fb5cf38658369815f686c88b64
avinashpatel45 <156068712+avinashpatel45@users.noreply.github.com>
2025-12-27 12:05:05 +0530

    Add files via upload

---
+++ Data/ProductDbCotext/ProductDbCotext.cs
@@ -0,0 +1,11 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.ProductDbCotext
+{
+    public class ProductDbCotext(DbContextOptions dbContextOptions) : DbContext (dbContextOptions)
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
